32950dcdfc2497e5e61016a96a4eae1d68bc0dba
achagal <achagal@umich.edu>
2024-12-23 16:15:23 -0600

    initial tauri commit

--- music-tool/backend/src/main.rs
+++ music-tool/backend/src/main.rs
@@ -1,18 +1,5 @@
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![upload_files, download_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-#[tauri::command]
-fn upload_files() {
-    // Logic to handle file upload to GitHub
-    println!("Uploading files...");
-}
-
-#[tauri::command]
-fn download_files() {
-    // Logic to handle file download from GitHub
-    println!("Downloading files...");
 }
